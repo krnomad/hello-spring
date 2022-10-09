def61f1b8587bf6ba2c13c59d6a878ed7c1c81f1
jong won Kang <krnomad@gmail.com>
2022-10-10 07:11:13 +0900

    implement & config JDBC

--- src/main/groovy/hello/hellospring/SpringConfig.groovy
+++ src/main/groovy/hello/hellospring/SpringConfig.groovy
@@ -1,13 +1,25 @@
 package hello.hellospring
 
+import hello.hellospring.repository.JdbcMemberRepository
 import hello.hellospring.repository.MemberRepository
 import hello.hellospring.repository.MemoryMemberRepository
 import hello.hellospring.service.MemberService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+import javax.sql.DataSource
+
 @Configuration
 class SpringConfig {
+
+    private dataSource
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource
+    }
+
     @Bean
     MemberService memberService() {
         new MemberService(memberRepository())
@@ -15,6 +27,7 @@ class SpringConfig {
 
     @Bean
     MemberRepository memberRepository() {
-        new MemoryMemberRepository()
+//        new MemoryMemberRepository()
+        new JdbcMemberRepository(dataSource)
     }
 }
